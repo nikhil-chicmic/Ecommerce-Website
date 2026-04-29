2c5f78c101ff3897eaf4df755665d7ec10d1cead
nikhil-chicmic <nikhil@chicmicstudios.in>
2026-04-29 11:23:55 +0530

    fix vite issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  preview: {
+    host: true, // allows 0.0.0.0
+    port: 4173,
+    allowedHosts: ["lumicaa.onrender.com"],
+  },
+});
